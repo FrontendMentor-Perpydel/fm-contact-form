994edd00e9dc5ba9af487b2282cc14fddc270c8f
Perpy-del <perpetualmeninwa@gmail.com>
2024-05-27 10:12:57 +0100

    style: :lipstick: customize the style to fit the style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '250px',
+      smd: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'lightGreen': 'hsl(148, 38%, 91%)',
+      'mediumGreen': 'hsl(169, 82%, 27%)',
+      'fmRed': 'hsl(0, 66%, 54%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'mediumGrey': 'hsl(186, 15%, 59%)',
+      'darkGrey': 'hsl(187, 24%, 22%)',
+    },
     extend: {},
   },
   plugins: [
