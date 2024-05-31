09a77db5f94b8dd51740ca89ee491619e46cf517
Perpy-del <perpetualmeninwa@gmail.com>
2024-05-31 22:59:58 +0100

    feat: Form validation and submission

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,40 +1,212 @@
+import { useReducer, useState } from 'react';
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  queryType: '',
+  message: '',
+  consent: false,
+  valid: true,
+  errorMessage: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    queryType: '',
+    message: '',
+    consent: '',
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'success':
+      return {
+        ...state,
+        [action.field]: action.value,
+        errorMessage: {
+          ...state.errorMessage,
+          [action.field]: '',
+        },
+      };
+    case 'failed':
+      return {
+        ...state,
+        errorMessage: {
+          ...state.errorMessage,
+          [action.field]: action.errorMessage,
+        },
+      };
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+}
+
 const FormComponent = () => {
+  // const [formValid, setFormValid] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [
+    { email, firstName, lastName, queryType, message, consent, errorMessage },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const validate = () => {
+    let valid = true;
+    const errors = {};
+
+    if (!firstName) {
+      errors.firstName = 'First name is required';
+      valid = false;
+    }
+    if (!lastName) {
+      errors.lastName = 'Last name is required';
+      valid = false;
+    }
+    if (!email) {
+      errors.email = 'Email is required';
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Email is invalid';
+      valid = false;
+    }
+    if (!message) {
+      errors.message = 'Message is required';
+      valid = false;
+    }
+    if (!consent) {
+      errors.termsAgreed = 'You must agree to the terms and conditions';
+      valid = false;
+    }
+
+    Object.keys(errors).forEach((field) => {
+      dispatch({ type: 'failed', field, errorMessage: errors[field] });
+    });
+
+    return valid;
+  };
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+
+    if (validate()) {
+      setIsLoading(false);
+      const formDetails = {
+        email,
+        firstName,
+        lastName,
+        queryType,
+        message,
+        consent,
+      };
+      localStorage.setItem('userDetails', JSON.stringify(formDetails));
+      dispatch({ type: 'reset' });
+    }
+
+  }
+
   return (
     <div className="bg-primary w-[50%] rounded-xl p-6">
       <h1 className="font-bold text-xl text-neutral pb-6">Contact Us</h1>
 
       {/* Form */}
-      <form className="text-neutral">
+      <form className="text-neutral" onSubmit={handleSubmitForm}>
         {/* Name Section */}
         <section className="flex gap-5 pb-5">
           {/* First Name */}
           <div className="flex flex-col w-full">
-            <label htmlFor="firstName" className="pb-1 font-medium">First Name *</label>
-            <input type="text" id="firstName" className="bg-primary border border-accent rounded-md p-2" />
+            <label htmlFor="firstName" className="pb-1 font-medium">
+              First Name *
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={e =>
+                dispatch({
+                  type: 'success',
+                  field: 'firstName',
+                  value: e.target.value,
+                })
+              }
+              className="bg-primary border border-accent rounded-md p-2"
+            />
+            {errorMessage.firstName && (
+              <span className="label-text-alt text-error pt-2">
+                {errorMessage.firstName}
+              </span>
+            )}
           </div>
 
           {/* Last Name */}
           <div className="flex flex-col w-full">
-            <label htmlFor="lastName" className="pb-1 font-medium">Last Name *</label>
-            <input type="text" id="lastName" className="bg-primary border border-accent rounded-md p-2" />
+            <label htmlFor="lastName" className="pb-1 font-medium">
+              Last Name *
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={e =>
+                dispatch({
+                  type: 'success',
+                  field: 'lastName',
+                  value: e.target.value,
+                })
+              }
+              className="bg-primary border border-accent rounded-md p-2"
+            />
+            {errorMessage.lastName && (
+              <span className="label-text-alt text-error pt-2">
+                {errorMessage.lastName}
+              </span>
+            )}
           </div>
         </section>
 
         {/* Email */}
         <div className="flex flex-col w-full pb-5">
-          <label htmlFor="email" className="pb-1 font-medium">Email Address *</label>
-          <input type="text" id="email" className="bg-primary border border-accent rounded-md p-2" />
+          <label htmlFor="email" className="pb-1 font-medium">
+            Email Address *
+          </label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={e =>
+              dispatch({
+                type: 'success',
+                field: 'email',
+                value: e.target.value,
+              })
+            }
+            className="bg-primary border border-accent rounded-md p-2"
+          />
+          {errorMessage.email && (
+            <span className="label-text-alt text-error pt-2">
+              {errorMessage.email}
+            </span>
+          )}
         </div>
 
         {/* Query */}
         <h3 className="pb-1 font-medium">Query Type *</h3>
-        <section className="flex gap-5 pb-5">
+        <section className="flex gap-5 pb-2">
           <div className="flex gap-2 pl-4 items-center p-2 border border-accent rounded-md w-full ">
             <input
               type="radio"
               id="general-enquiry"
-              name="query"
+              name="queryType"
               value="generalEnquiry"
+              checked={queryType === 'generalEnquiry'}
+              onChange={(e) =>
+                dispatch({
+                  type: 'success',
+                  field: 'queryType',
+                  value: e.target.value,
+                })
+              }
               className="radio radio-secondary"
             />
             <label htmlFor="general-enquiry">General Enquiry</label>
@@ -43,30 +215,87 @@ const FormComponent = () => {
             <input
               type="radio"
               id="support-request"
-              name="query"
+              name="queryType"
               value="supportRequest"
+              checked={queryType === 'supportRequest'}
+              onChange={(e) =>
+                dispatch({
+                  type: 'success',
+                  field: 'queryType',
+                  value: e.target.value,
+                })
+              }
               className="radio radio-secondary"
             />
             <label htmlFor="support-request">Support Request</label>
           </div>
         </section>
+        {errorMessage.queryType && (
+          <span className="label-text-alt text-error">
+            {errorMessage.queryType}
+          </span>
+        )}
 
         {/* Message */}
-        <div className="flex flex-col pb-7">
-          <label htmlFor="message" className="pb-1 font-medium">Message *</label>
-          <textarea name="message" id="message" className="bg-primary border border-accent rounded-md p-2" rows={3}></textarea>
+        <div className="flex flex-col pb-7 pt-5">
+          <label htmlFor="message" className="pb-1 font-medium">
+            Message *
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            className="bg-primary border border-accent rounded-md p-2"
+            rows={3}
+            value={message}
+            onChange={e =>
+              dispatch({
+                type: 'success',
+                field: 'message',
+                value: e.target.value,
+              })
+            }
+          ></textarea>
+          {errorMessage.message && (
+            <span className="label-text-alt text-error pt-2">
+              {errorMessage.message}
+            </span>
+          )}
         </div>
 
         {/* Consent checkbox */}
-        <div className="flex gap-3 items-center pb-7">
-          <input type="checkbox" id="consent" name="consent" value="consent" className="checkbox checkbox-sm rounded-md checkbox-secondary" />
+        <div className="flex gap-3 items-center">
+          <input
+            type="checkbox"
+            id="consent"
+            name="consent"
+            checked={consent}
+            onChange={e =>
+              dispatch({
+                type: 'success',
+                field: 'consent',
+                value: e.target.value,
+              })
+            }
+            className="checkbox checkbox-sm rounded-md checkbox-secondary"
+          />
           <label htmlFor="consent" className="font-medium">
             I consent to being contacted by the team *
           </label>
         </div>
+        {errorMessage.consent && (
+          <span className="label-text-alt text-error">
+            {errorMessage.consent}
+          </span>
+        )}
 
         {/* Submit button */}
-        <button type="submit" className="btn w-full btn-secondary text-lg">Submit</button>
+        <button type="submit" className="btn w-full btn-secondary text-lg mt-7">
+          {isLoading ? (
+            <span className="loading loading-spinner text-warning loading-lg"></span>
+          ) : (
+            'Submit'
+          )}
+        </button>
       </form>
     </div>
   );
--- src/components/ThemeController.jsx
+++ src/components/ThemeController.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const ThemeController = () => {
+  const [selectedTheme, setSelectedTheme] = useState('default');
+
+  const handleThemeChange = (event) => {
+    setSelectedTheme(event.target.value);
+  };
+
   return (
     <div className="dropdown mb-1 flex justify-end">
       <div tabIndex={0} role="button" className="btn m-1">
@@ -12,7 +20,7 @@ const ThemeController = () => {
         >
           <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
         </svg>
-      </div>
+      </div> 
       <ul
         tabIndex={0}
         className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-30"
@@ -24,6 +32,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Default"
             value="default"
+            checked={selectedTheme === 'default'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -33,6 +43,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Dark"
             value="dark"
+            checked={selectedTheme === 'dark'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -42,6 +54,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Bumblebee"
             value="bumblebee"
+            checked={selectedTheme === 'bumblebee'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -51,6 +65,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Emerald"
             value="emerald"
+            checked={selectedTheme === 'emerald'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -60,6 +76,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Corporate"
             value="corporate"
+            checked={selectedTheme === 'corporate'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -69,6 +87,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Synthwave"
             value="synthwave"
+            checked={selectedTheme === 'synthwave'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -78,6 +98,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Retro"
             value="retro"
+            checked={selectedTheme === 'retro'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -87,6 +109,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Cyberpunk"
             value="cyberpunk"
+            checked={selectedTheme === 'cyberpunk'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -96,6 +120,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Valentine"
             value="valentine"
+            checked={selectedTheme === 'valentine'}
+            onChange={handleThemeChange}
           />
         </li>
         <li>
@@ -105,6 +131,8 @@ const ThemeController = () => {
             className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
             aria-label="Halloween"
             value="halloween"
+            checked={selectedTheme === 'halloween'}
+            onChange={handleThemeChange}
           />
         </li>
       </ul>
